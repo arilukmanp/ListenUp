ea1485de3139eab8776af5738e818010148613ae
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-18 15:58:34 +0700

    Initial Commit

---
+++ ListenUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListenUp
+//
+//  Created by Ari on 18/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
