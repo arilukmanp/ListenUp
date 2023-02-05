c837cf379e73c57cdf7dab49d4f54e76cb6002fb
Ari Lukman P <ari.lukman.p@gmail.com>
2023-02-05 17:51:25 +0700

    Implement input validation in login

--- ListenUp/Modules/Auth/SignInViewController.swift
+++ ListenUp/Modules/Auth/SignInViewController.swift
@@ -12,29 +12,82 @@ class SignInViewController: UIViewController {
     // MARK: - Properties
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
-
+    @IBOutlet weak var showPasswordButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupUI()
+        setup()
     }
     
     
     // MARK: - Helper
-    func setupUI() {
-        tfEmail.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
-        tfPassword.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+    func setup() {
+        tfEmail.delegate = self
+        tfPassword.delegate = self
+        
+        tfPassword.isSecureTextEntry = true
+        signinButton.isEnabled = false
+        
+        tfEmail.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [.foregroundColor: UIColor.gray])
+        tfPassword.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor.gray])
+    }
+    
+    func isEmailValid(_ email: String?) -> Bool {
+        let value = email ?? tfEmail.text ?? ""
+        return value.isValidEmail
+    }
+    
+    func isPasswordValid(_ password: String?) -> Bool {
+        let value = password ?? tfPassword.text ?? ""
+        return value.isValidPassword
     }
     
     
     // MARK: - Action
+    @IBAction func showPasswordButtonTapped(_ sender: Any) {
+        let isSecuredText = tfPassword.isSecureTextEntry
+        let eye = UIImage(systemName: "eye")
+        let eyeSlash = UIImage(systemName: "eye.slash")
+        
+        tfPassword.isSecureTextEntry = !isSecuredText
+        if let image = isSecuredText ? eyeSlash : eye {
+            showPasswordButton.setImage(image, for: .normal)
+        }
+    }
+    
     @IBAction func signInButtonTapped(_ sender: Any) {
         showMainViewController()
     }
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
         showSignUpViewController()
+    }
+}
+
+
+// MARK: - Extension: Textfield Delegate
+extension SignInViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var isEmailValid = false
+        var isPasswordValid = false
+        
+        let input = NSString(string: textField.text ?? "").replacingCharacters(in: range, with: string)
+        
+        if textField == tfEmail {
+            isEmailValid = self.isEmailValid(input)
+            isPasswordValid = self.isPasswordValid(nil)
+        } else {
+            isPasswordValid = self.isPasswordValid(input)
+            isEmailValid = self.isEmailValid(nil)
+        }
+        
+        signinButton.isEnabled = isEmailValid && isPasswordValid
+        
+        return true
     }
 }
