b1b88cf91167366b6c761ac474bdedff7be38e01
Ari Lukman P <ari.lukman.p@gmail.com>
2023-02-06 12:54:26 +0700

    Add Episode module

---
+++ ListenUp/Modules/Episode/EpisodesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesViewController.swift
+//  ListenUp
+//
+//  Created by Ari on 06/02/23.
+//
+
+import UIKit
+
+class EpisodesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
