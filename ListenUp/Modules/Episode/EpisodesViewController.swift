9e1b59985a4076f65634c70132972c6c5465ee4c
Ari Lukman P <ari.lukman.p@gmail.com>
2023-02-10 21:08:24 +0700

    Assign data to episode view

--- ListenUp/Modules/Episode/EpisodesViewController.swift
+++ ListenUp/Modules/Episode/EpisodesViewController.swift
@@ -6,18 +6,66 @@
 //
 
 import UIKit
+import FeedKit
+import Kingfisher
+
 
 class EpisodesViewController: UIViewController {
 
     // MARK: - Properties
     @IBOutlet weak var episodeTableView: UITableView!
     
+    var podcast: Podcast!
+    var episodes: [RSSFeedItem] = []
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
+        loadEpisodes()
     }
+    
+    
+    // MARK: - Helper
+    func setup() {
+        title = podcast.trackName
+        
+        episodeTableView.dataSource = self
+    }
+    
+    func loadEpisodes() {
+        APIService.shared.loadEpisodes(url: podcast.feedUrl) { episodes in
+            self.episodes = episodes
+            self.episodeTableView.reloadData()
+        }
+    }
+}
+
+
+// MARK: - Extension: Table Datasource
+extension EpisodesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "episode_cell_id", for: indexPath) as! EposodeViewCell
+        
+        let episode = self.episodes[indexPath.row]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyy HH:mm"
+        
+        cell.thumbImageView.kf.setImage(with: URL(string: episode.iTunes?.iTunesImage?.attributes?.href ?? ""))
+        cell.dateLabel.text = episode.pubDate != nil ? dateFormatter.string(from: episode.pubDate!) : ""
+        cell.titleLabel.text = episode.title
+        cell.descriptionLabel.text = episode.iTunes?.iTunesSubtitle ?? episode.description
+        
+        return cell
+    }
+    
     
 }
